6dce03916e4f7536a18f265e05eae5ac80f0010e
Max Meldrum <max@meldrum.se>
2016-06-15 18:52:14 +0200

    Models changed to avoid import at top of file

--- api/app.py
+++ api/app.py
@@ -1,22 +1,19 @@
 from flask import Flask
 from flask_restful import Api
-from flask_sqlalchemy import SQLAlchemy
 from resource.user import UserAPI
+from models import db
 
 import config
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = config.DB_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+db.init_app(app)
 
 app.debug = config.DEBUG
 api = Api(app)
-
-import models
 
 api.add_resource(UserAPI, '/user')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
